714d3801ad5b8c73e601497ecba44f9e981f50a7
CJ <cj.wayland@usu.edu>
2024-04-09 21:34:20 -0600

    touch the other thing

--- main.c
+++ main.c
@@ -209,7 +209,7 @@ int main() {
         // Use NORAD ID to make API command
         // Puts TLE in TLE.txt
         snprintf(TLE_Command, sizeof(TLE_Command),
-                 "curl --limit-rate 100K --cookie cookies.txt https://www.space-track.org/basicspacedata/query/class/gp/format/tle/NORAD_CAT_ID/%s  > TLE.txt",
+                 "curl --limit-rate 100K --cookie cookies.txt https://www.space-track.org/basicspacedata/query/class/gp/format/tle/NORAD_CAT_ID/%s  > /home/astra/ASTRA/TLE.txt",
                  buffer);
         system(TLE_Command);
 
