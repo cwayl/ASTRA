3b9e4a336c2ff73f555abf21ea97835561b1285f
CJ <cj.wayland@usu.edu>
2024-04-10 18:02:21 -0600

    Login Information

--- Password.sh
+++ Password.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # show an inputbox
-dialog --title "Space-Track.org Credentials" \
+dialog --title "Space-Track.org Login Information" \
        --backtitle "ASTRA" \
        --nocancel \
        --inputbox "Enter your Space-Track.org password " 8 60 "$1" 2>/home/astra/ASTRA/text.txt
