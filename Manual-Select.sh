56ecd04da3b8f8cfda9e08f4336bb64b5241c490
CJ <cj.wayland@usu.edu>
2024-04-17 17:11:14 -0600

    manual select wording changes

--- Manual-Select.sh
+++ Manual-Select.sh
@@ -5,7 +5,7 @@ dialog --title "Mode Select" \
        --backtitle "ASTRA" \
        --nocancel \
        --menu "Select TLE Input Mode" 12 45 25 \
-       1 "Use NORAD ID (Space-Track.org)" \
-       2 "Manually Enter New TLE" \
-       3 "Use current TLE" \
+       1 "Using NORAD ID (Space-Track.org)" \
+       2 "Manually Entering New TLE" \
+       3 "Using current TLE" \
        2>/home/astra/ASTRA/text.txt
